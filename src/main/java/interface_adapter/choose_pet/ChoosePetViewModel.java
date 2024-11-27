8fb2350089115d1626328cf545c2b70aed76e4ed
amandahe <xiying.he@mail.utoronto.ca>
2024-11-27 18:55:54 -0500

    pet view model

--- src/main/java/interface_adapter/choose_pet/ChoosePetViewModel.java
+++ src/main/java/interface_adapter/choose_pet/ChoosePetViewModel.java
@@ -1,4 +1,47 @@
 package interface_adapter.choose_pet;
 
-public class ChoosePetViewModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import interface_adapter.ViewModel;
+
+/**
+ * The View Model for the Choose Pet View.
+ */
+public class ChoosePetViewModel extends ViewModel<ChoosePetState> {
+
+    public static final String TITLE_LABEL = "Choose Pet Screen";
+    public static final String PET_LABEL = "Select Your Pet";
+    public static final String CONFIRM_BUTTON_LABEL = "Confirm Selection";
+    public static final String ERROR_LABEL = "Error: ";
+
+    private ChoosePetState state = new ChoosePetState();
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public ChoosePetViewModel() {
+        super("choose pet");
+    }
+
+    @Override
+    public void setState(ChoosePetState state) {
+        final ChoosePetState oldState = this.state;
+        this.state = state;
+        support.firePropertyChange("state", oldState, state);
+    }
+
+    public ChoosePetState getState() {
+        return state;
+    }
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
+
